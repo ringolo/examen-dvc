6bcad51cd5c29fd4ee7fae6eaa0e29963327fa3a
bh <bh@email.com>
2025-08-30 19:18:48 +0000

    added file extensions to clean.sh

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -3,9 +3,9 @@ script_path="$(readlink -f "$0")"
 script_dir="$(dirname "$script_path")"
 project_folder="$(dirname "$script_dir")"
 
-rm $project_folder/data/raw_data/*
-rm $project_folder/data/processed_data/*
-rm $project_folder/logs/*
+rm $project_folder/data/raw_data/*.csv
+rm $project_folder/data/processed_data/*.csv
+rm $project_folder/logs/*.txt
 rmdir $project_folder/logs
-rm $project_folder/metrics/*
-rm $project_folder/models/*
+rm $project_folder/metrics/*.json
+rm $project_folder/models/*.pkl
